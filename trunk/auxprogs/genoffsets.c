svn2github/vex

commit 8636c95ea6a13414585a58fea74a24e1915971c2

Add support for ARMv8 AArch64 (the 64 bit ARM instruction set): integer and FP instructions.
git-svn-id: svn://svn.valgrind.org/vex@2803 8f6e269a-dfd6-0310-a8e1-e2731360e62c

trunk/auxprogs/genoffsets.c
@@ -51,6 +51,7 @@
 #include "../pub/libvex_guest_ppc32.h"
 #include "../pub/libvex_guest_ppc64.h"
 #include "../pub/libvex_guest_arm.h"
+#include "../pub/libvex_guest_arm64.h"
 #include "../pub/libvex_guest_s390x.h"
 #include "../pub/libvex_guest_mips32.h"
 #include "../pub/libvex_guest_mips64.h"
@@ -158,6 +159,19 @@ void foo ( void )
    GENOFFSET(ARM,arm,R13);
    GENOFFSET(ARM,arm,R14);
    GENOFFSET(ARM,arm,R15T);
+
+   // arm64
+   GENOFFSET(ARM64,arm64,X0);
+   GENOFFSET(ARM64,arm64,X1);
+   GENOFFSET(ARM64,arm64,X2);
+   GENOFFSET(ARM64,arm64,X3);
+   GENOFFSET(ARM64,arm64,X4);
+   GENOFFSET(ARM64,arm64,X5);
+   GENOFFSET(ARM64,arm64,X6);
+   GENOFFSET(ARM64,arm64,X7);
+   GENOFFSET(ARM64,arm64,X8);
+   GENOFFSET(ARM64,arm64,SP);
+   GENOFFSET(ARM64,arm64,PC);
 
    // s390x
    GENOFFSET(S390X,s390x,r2);

trunk/priv/guest_s390_helpers.c
@@ -154,11 +154,11 @@ LibVEX_GuestS390X_initialise(VexGuestS390XState *state)
 Bool
 guest_s390x_state_requires_precise_mem_exns(Int minoff, Int maxoff)
 {
-   Int lr_min = S390X_GUEST_OFFSET(guest_LR);
+   Int lr_min = S390X_GUEST_OFFSET(guest_s390_LR);
    Int lr_max = lr_min + 8 - 1;
-   Int sp_min = S390X_GUEST_OFFSET(guest_SP);
+   Int sp_min = S390X_GUEST_OFFSET(guest_s390_SP);
    Int sp_max = sp_min + 8 - 1;
-   Int fp_min = S390X_GUEST_OFFSET(guest_FP);
+   Int fp_min = S390X_GUEST_OFFSET(guest_s390_FP);
    Int fp_max = fp_min + 8 - 1;
    Int ia_min = S390X_GUEST_OFFSET(guest_IA);
    Int ia_max = ia_min + 8 - 1;
@@ -203,11 +203,11 @@ VexGuestLayout s390xGuest_layout = {
    .total_sizeB = sizeof(VexGuestS390XState),
 
    /* Describe the stack pointer. */
-   .offset_SP = S390X_GUEST_OFFSET(guest_SP),
+   .offset_SP = S390X_GUEST_OFFSET(guest_s390_SP),
    .sizeof_SP = 8,
 
    /* Describe the frame pointer. */
-   .offset_FP = S390X_GUEST_OFFSET(guest_FP),
+   .offset_FP = S390X_GUEST_OFFSET(guest_s390_FP),
    .sizeof_FP = 8,
 
    /* Describe the instruction pointer. */

trunk/pub/libvex_guest_arm64.h
@@ -1,0 +1,190 @@
+
+/*---------------------------------------------------------------*/
+/*--- begin                              libvex_guest_arm64.h ---*/
+/*---------------------------------------------------------------*/
+
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2013-2013 OpenWorks
+      info@open-works.net
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+#ifndef __LIBVEX_PUB_GUEST_ARM64_H
+#define __LIBVEX_PUB_GUEST_ARM64_H
+
+#include "libvex_basictypes.h"
+
+
+/*---------------------------------------------------------------*/
+/*--- Vex's representation of the ARM64 CPU state.            ---*/
+/*---------------------------------------------------------------*/
+
+typedef
+   struct {
+      /* Event check fail addr and counter. */
+      /* 0 */  ULong host_EvC_FAILADDR;
+      /* 8 */  UInt  host_EvC_COUNTER;
+      /* 12 */ UInt  pad0;
+      /* 16 */
+      ULong guest_X0;
+      ULong guest_X1;
+      ULong guest_X2;
+      ULong guest_X3;
+      ULong guest_X4;
+      ULong guest_X5;
+      ULong guest_X6;
+      ULong guest_X7;
+      ULong guest_X8;
+      ULong guest_X9;
+      ULong guest_X10;
+      ULong guest_X11;
+      ULong guest_X12;
+      ULong guest_X13;
+      ULong guest_X14;
+      ULong guest_X15;
+      ULong guest_X16;
+      ULong guest_X17;
+      ULong guest_X18;
+      ULong guest_X19;
+      ULong guest_X20;
+      ULong guest_X21;
+      ULong guest_X22;
+      ULong guest_X23;
+      ULong guest_X24;
+      ULong guest_X25;
+      ULong guest_X26;
+      ULong guest_X27;
+      ULong guest_X28;
+      ULong guest_X29;
+      ULong guest_X30;     /* link register */
+      ULong guest_SP;
+      ULong guest_PC;
+
+      /* 4-word thunk used to calculate N(sign) Z(zero) C(carry,
+         unsigned overflow) and V(signed overflow) flags. */
+      ULong guest_CC_OP;
+      ULong guest_CC_DEP1;
+      ULong guest_CC_DEP2;
+      ULong guest_CC_NDEP;
+
+      /* User-space thread register? */
+      ULong guest_TPIDR_EL0;
+
+      /* FP/SIMD state */
+      U128 guest_Q0;
+      U128 guest_Q1;
+      U128 guest_Q2;
+      U128 guest_Q3;
+      U128 guest_Q4;
+      U128 guest_Q5;
+      U128 guest_Q6;
+      U128 guest_Q7;
+      U128 guest_Q8;
+      U128 guest_Q9;
+      U128 guest_Q10;
+      U128 guest_Q11;
+      U128 guest_Q12;
+      U128 guest_Q13;
+      U128 guest_Q14;
+      U128 guest_Q15;
+      U128 guest_Q16;
+      U128 guest_Q17;
+      U128 guest_Q18;
+      U128 guest_Q19;
+      U128 guest_Q20;
+      U128 guest_Q21;
+      U128 guest_Q22;
+      U128 guest_Q23;
+      U128 guest_Q24;
+      U128 guest_Q25;
+      U128 guest_Q26;
+      U128 guest_Q27;
+      U128 guest_Q28;
+      U128 guest_Q29;
+      U128 guest_Q30;
+      U128 guest_Q31;
+
+      /* Various pseudo-regs mandated by Vex or Valgrind. */
+      /* Emulation notes */
+      UInt guest_EMNOTE;
+
+      /* For clflush: record start and length of area to invalidate */
+      ULong guest_TISTART;
+      ULong guest_TILEN;
+
+      /* Used to record the unredirected guest address at the start of
+         a translation whose start has been redirected.  By reading
+         this pseudo-register shortly afterwards, the translation can
+         find out what the corresponding no-redirection address was.
+         Note, this is only set for wrap-style redirects, not for
+         replace-style ones. */
+      ULong guest_NRADDR;
+
+      /* Needed for Darwin (but mandated for all guest architectures):
+         program counter at the last syscall insn (int 0x80/81/82,
+         sysenter, syscall, svc).  Used when backing up to restart a
+         syscall that has been interrupted by a signal. */
+      ULong guest_IP_AT_SYSCALL;
+
+      /* The complete FPCR.  Default value seems to be zero.  We
+         ignore all bits except 23 and 22, which are the rounding
+         mode.  The guest is unconstrained in what values it can write
+         to and read from this register, but the emulation only takes
+         note of bits 23 and 22. */
+      UInt  guest_FPCR;
+
+      /* The complete FPSR.  As with FPCR, the guest may write and
+         read any values here, and the emulation ignores it, with the
+         exception of bit 27 (QC, the sticky saturation bit) which
+         does get set when required. */
+      UInt  guest_FPSR;
+
+      /* Padding to make it have an 32-aligned size */
+      UInt  pad_end_0;
+      ULong pad_end_1;
+   }
+   VexGuestARM64State;
+
+
+/*---------------------------------------------------------------*/
+/*--- Utility functions for ARM64 guest stuff.                ---*/
+/*---------------------------------------------------------------*/
+
+/* ALL THE FOLLOWING ARE VISIBLE TO LIBRARY CLIENT */
+
+/* Initialise all guest ARM64 state. */
+
+extern
+void LibVEX_GuestARM64_initialise ( /*OUT*/VexGuestARM64State* vex_state );
+
+/* Calculate the ARM64 flag state from the saved data, in the format
+   32x0:n:z:c:v:28x0. */
+extern
+ULong LibVEX_GuestARM64_get_nzcv ( /*IN*/
+                                   const VexGuestARM64State* vex_state );
+
+#endif /* ndef __LIBVEX_PUB_GUEST_ARM64_H */
+
+
+/*---------------------------------------------------------------*/
+/*---                                    libvex_guest_arm64.h ---*/
+/*---------------------------------------------------------------*/

trunk/Makefile-gcc
@@ -50,12 +50,14 @@ LIB_OBJS = 	priv/ir_defs.o                          \
 		priv/host_x86_defs.o			\
 		priv/host_amd64_defs.o			\
 		priv/host_arm_defs.o			\
+		priv/host_arm64_defs.o			\
 		priv/host_ppc_defs.o			\
 		priv/host_s390_defs.o			\
 		priv/host_mips_defs.o			\
 		priv/host_x86_isel.o			\
 		priv/host_amd64_isel.o			\
 		priv/host_arm_isel.o			\
+		priv/host_arm64_isel.o			\
 		priv/host_ppc_isel.o			\
 		priv/host_s390_isel.o			\
 		priv/host_mips_isel.o			\
@@ -70,12 +72,14 @@ LIB_OBJS = 	priv/ir_defs.o                          \
 		priv/guest_x86_helpers.o		\
 		priv/guest_amd64_helpers.o		\
 		priv/guest_arm_helpers.o		\
+		priv/guest_arm64_helpers.o		\
 		priv/guest_ppc_helpers.o		\
 		priv/guest_s390_helpers.o		\
 		priv/guest_mips_helpers.o		\
 		priv/guest_x86_toIR.o			\
 		priv/guest_amd64_toIR.o			\
 		priv/guest_arm_toIR.o			\
+		priv/guest_arm64_toIR.o			\
 		priv/guest_ppc_toIR.o                   \
 		priv/guest_s390_toIR.o			\
 		priv/guest_mips_toIR.o
@@ -194,6 +198,12 @@ TAG-amd64-darwin:
 	if [ ! -f TAG-amd64-darwin ] ; then rm -f $(LIB_OBJS) TAG-* libvex.a ; fi
 	touch TAG-amd64-darwin
 
+libvex-arm64-linux.a: TAG-arm64-linux libvex.a
+	mv -f libvex.a libvex-arm64-linux.a
+TAG-arm64-linux:
+	if [ ! -f TAG-arm64-linux ] ; then rm -f $(LIB_OBJS) TAG-* libvex.a ; fi
+	touch TAG-arm64-linux
+
 
 clean:
 	rm -f $(LIB_OBJS) *.a vex test_main.o TAG-* \
@@ -273,6 +283,10 @@ priv/host_arm_defs.o: $(ALL_HEADERS) priv/host_arm_defs.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_arm_defs.o \
 					 -c priv/host_arm_defs.c
 
+priv/host_arm64_defs.o: $(ALL_HEADERS) priv/host_arm64_defs.c
+	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_arm64_defs.o \
+					 -c priv/host_arm64_defs.c
+
 priv/host_ppc_defs.o: $(ALL_HEADERS) priv/host_ppc_defs.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_ppc_defs.o \
 					 -c priv/host_ppc_defs.c
@@ -296,6 +310,10 @@ priv/host_amd64_isel.o: $(ALL_HEADERS) priv/host_amd64_isel.c
 priv/host_arm_isel.o: $(ALL_HEADERS) priv/host_arm_isel.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_arm_isel.o \
 					 -c priv/host_arm_isel.c
+
+priv/host_arm64_isel.o: $(ALL_HEADERS) priv/host_arm64_isel.c
+	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_arm64_isel.o \
+					 -c priv/host_arm64_isel.c
 
 priv/host_ppc_isel.o: $(ALL_HEADERS) priv/host_ppc_isel.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/host_ppc_isel.o \
@@ -361,9 +379,17 @@ priv/guest_arm_helpers.o: $(ALL_HEADERS) priv/guest_arm_helpers.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/guest_arm_helpers.o \
 					 -c priv/guest_arm_helpers.c
 
+priv/guest_arm64_helpers.o: $(ALL_HEADERS) priv/guest_arm64_helpers.c
+	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/guest_arm64_helpers.o \
+					 -c priv/guest_arm64_helpers.c
+
 priv/guest_arm_toIR.o: $(ALL_HEADERS) priv/guest_arm_toIR.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/guest_arm_toIR.o \
 					 -c priv/guest_arm_toIR.c
+
+priv/guest_arm64_toIR.o: $(ALL_HEADERS) priv/guest_arm64_toIR.c
+	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/guest_arm64_toIR.o \
+					 -c priv/guest_arm64_toIR.c
 
 priv/guest_ppc_helpers.o: $(ALL_HEADERS) priv/guest_ppc_helpers.c
 	$(CC) $(CCFLAGS) $(ALL_INCLUDES) -o priv/guest_ppc_helpers.o \

trunk/priv/guest_arm64_defs.h
@@ -1,0 +1,244 @@
+
+/*---------------------------------------------------------------*/
+/*--- begin                                guest_arm64_defs.h ---*/
+/*---------------------------------------------------------------*/
+/*
+   This file is part of Valgrind, a dynamic binary instrumentation
+   framework.
+
+   Copyright (C) 2013-2013 OpenWorks
+      info@open-works.net
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful, but
+   WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+   02110-1301, USA.
+
+   The GNU General Public License is contained in the file COPYING.
+*/
+
+#ifndef __VEX_GUEST_ARM64_DEFS_H
+#define __VEX_GUEST_ARM64_DEFS_H
+
+#include "libvex_basictypes.h"
+#include "guest_generic_bb_to_IR.h"     // DisResult
+
+/*---------------------------------------------------------*/
+/*--- arm64 to IR conversion                            ---*/
+/*---------------------------------------------------------*/
+
+/* Convert one ARM64 insn to IR.  See the type DisOneInstrFn in
+   bb_to_IR.h. */
+extern
+DisResult disInstr_ARM64 ( IRSB*        irbb,
+                           Bool         (*resteerOkFn) ( void*, Addr64 ),
+                           Bool         resteerCisOk,
+                           void*        callback_opaque,
+                           UChar*       guest_code,
+                           Long         delta,
+                           Addr64       guest_IP,
+                           VexArch      guest_arch,
+                           VexArchInfo* archinfo,
+                           VexAbiInfo*  abiinfo,
+                           Bool         host_bigendian,
+                           Bool         sigill_diag );
+
+/* Used by the optimiser to specialise calls to helpers. */
+extern
+IRExpr* guest_arm64_spechelper ( const HChar* function_name,
+                                 IRExpr** args,
+                                 IRStmt** precedingStmts,
+                                 Int      n_precedingStmts );
+
+/* Describes to the optimser which part of the guest state require
+   precise memory exceptions.  This is logically part of the guest
+   state description. */
+extern 
+Bool guest_arm64_state_requires_precise_mem_exns ( Int, Int );
+
+extern
+VexGuestLayout arm64Guest_layout;
+
+
+/*---------------------------------------------------------*/
+/*--- arm64 guest helpers                               ---*/
+/*---------------------------------------------------------*/
+
+/* --- CLEAN HELPERS --- */
+
+/* Calculate NZCV from the supplied thunk components, in the positions
+   they appear in the CPSR, viz bits 31:28 for N Z C V respectively.
+   Returned bits 63:32 and 27:0 are zero. */
+extern 
+ULong arm64g_calculate_flags_nzcv ( ULong cc_op, ULong cc_dep1,
+                                    ULong cc_dep2, ULong cc_dep3 );
+
+//ZZ /* Calculate the C flag from the thunk components, in the lowest bit
+//ZZ    of the word (bit 0). */
+//ZZ extern 
+//ZZ UInt armg_calculate_flag_c ( UInt cc_op, UInt cc_dep1,
+//ZZ                              UInt cc_dep2, UInt cc_dep3 );
+//ZZ 
+//ZZ /* Calculate the V flag from the thunk components, in the lowest bit
+//ZZ    of the word (bit 0). */
+//ZZ extern 
+//ZZ UInt armg_calculate_flag_v ( UInt cc_op, UInt cc_dep1,
+//ZZ                              UInt cc_dep2, UInt cc_dep3 );
+//ZZ 
+/* Calculate the specified condition from the thunk components, in the
+   lowest bit of the word (bit 0). */
+extern 
+ULong arm64g_calculate_condition ( /* ARM64Condcode << 4 | cc_op */
+                                   ULong cond_n_op ,
+                                   ULong cc_dep1,
+                                   ULong cc_dep2, ULong cc_dep3 );
+
+//ZZ /* Calculate the QC flag from the thunk components, in the lowest bit
+//ZZ    of the word (bit 0). */
+//ZZ extern 
+//ZZ UInt armg_calculate_flag_qc ( UInt resL1, UInt resL2,
+//ZZ                               UInt resR1, UInt resR2 );
+
+
+/*---------------------------------------------------------*/
+/*--- Condition code stuff                              ---*/
+/*---------------------------------------------------------*/
+
+/* Flag masks.  Defines positions of flag bits in the NZCV
+   register. */
+#define ARM64G_CC_SHIFT_N  31
+#define ARM64G_CC_SHIFT_Z  30
+#define ARM64G_CC_SHIFT_C  29
+#define ARM64G_CC_SHIFT_V  28
+//ZZ #define ARMG_CC_SHIFT_Q  27
+//ZZ 
+//ZZ #define ARMG_CC_MASK_N    (1 << ARMG_CC_SHIFT_N)
+//ZZ #define ARMG_CC_MASK_Z    (1 << ARMG_CC_SHIFT_Z)
+//ZZ #define ARMG_CC_MASK_C    (1 << ARMG_CC_SHIFT_C)
+//ZZ #define ARMG_CC_MASK_V    (1 << ARMG_CC_SHIFT_V)
+//ZZ #define ARMG_CC_MASK_Q    (1 << ARMG_CC_SHIFT_Q)
+
+/* Flag thunk descriptors.  A four-word thunk is used to record
+   details of the most recent flag-setting operation, so NZCV can
+   be computed later if needed.
+
+   The four words are:
+
+      CC_OP, which describes the operation.
+
+      CC_DEP1, CC_DEP2, CC_NDEP.  These are arguments to the
+         operation.  We want set up the mcx_masks in flag helper calls
+         involving these fields so that Memcheck "believes" that the
+         resulting flags are data-dependent on both CC_DEP1 and
+         CC_DEP2.  Hence the name DEP.
+
+   When building the thunk, it is always necessary to write words into
+   CC_DEP1/2 and NDEP, even if those args are not used given the CC_OP
+   field.  This is important because otherwise Memcheck could give
+   false positives as it does not understand the relationship between
+   the CC_OP field and CC_DEP1/2/NDEP, and so believes that the
+   definedness of the stored flags always depends on all 3 DEP values.
+
+   A summary of the field usages is:
+
+   OP                DEP1              DEP2              DEP3
+   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+   OP_COPY           curr_NZCV:28x0    unused            unused
+   OP_ADD32          argL              argR              unused
+   OP_ADD64          argL              argR              unused
+   OP_SUB32          argL              argR              unused
+   OP_SUB64          argL              argR              unused
+//ZZ    OP_ADC            argL              argR              31x0:old_C
+//ZZ    OP_SBB            argL              argR              31x0:old_C
+   OP_LOGIC32        result            unused            unused
+   OP_LOGIC64        result            unused            unused
+//ZZ    OP_MUL            result            unused            30x0:old_C:old_V
+//ZZ    OP_MULL           resLO32           resHI32           30x0:old_C:old_V
+//ZZ */
+
+enum {
+   ARM64G_CC_OP_COPY=0,   /* DEP1 = NZCV in 31:28, DEP2 = 0, DEP3 = 0
+                             just copy DEP1 to output */
+
+   ARM64G_CC_OP_ADD32,    /* DEP1 = argL (Rn), DEP2 = argR (shifter_op),
+                             DEP3 = 0 */
+
+   ARM64G_CC_OP_ADD64,    /* DEP1 = argL (Rn), DEP2 = argR (shifter_op),
+                             DEP3 = 0 */
+
+   ARM64G_CC_OP_SUB32,    /* DEP1 = argL (Rn), DEP2 = argR (shifter_op),
+                             DEP3 = 0 */
+
+   ARM64G_CC_OP_SUB64,    /* DEP1 = argL (Rn), DEP2 = argR (shifter_op),
+                             DEP3 = 0 */
+
+//ZZ    ARMG_CC_OP_ADC,     /* DEP1 = argL (Rn), DEP2 = arg2 (shifter_op),
+//ZZ                           DEP3 = oldC (in LSB) */
+//ZZ 
+//ZZ    ARMG_CC_OP_SBB,     /* DEP1 = argL (Rn), DEP2 = arg2 (shifter_op),
+//ZZ                           DEP3 = oldC (in LSB) */
+
+   ARM64G_CC_OP_LOGIC32,  /* DEP1 = result, DEP2 = 0, DEP3 = 0 */
+   ARM64G_CC_OP_LOGIC64,  /* DEP1 = result, DEP2 = 0, DEP3 = 0 */
+
+//ZZ    ARMG_CC_OP_MUL,     /* DEP1 = result, DEP2 = 0, DEP3 = oldC:old_V
+//ZZ                           (in bits 1:0) */
+//ZZ 
+//ZZ    ARMG_CC_OP_MULL,    /* DEP1 = resLO32, DEP2 = resHI32, DEP3 = oldC:old_V
+//ZZ                           (in bits 1:0) */
+
+   ARM64G_CC_OP_NUMBER
+};
+
+/* XXXX because of the calling conventions for
+   arm64g_calculate_condition, all these OP values MUST be in the range
+   0 .. 15 only (viz, 4-bits). */
+
+
+
+/* Defines conditions which we can ask for */
+
+typedef
+   enum {
+      ARM64CondEQ = 0,  /* equal                         : Z=1 */
+      ARM64CondNE = 1,  /* not equal                     : Z=0 */
+
+      ARM64CondCS = 2,  /* >=u (higher or same) (aka HS) : C=1 */
+      ARM64CondCC = 3,  /* <u  (lower)          (aka LO) : C=0 */
+
+      ARM64CondMI = 4,  /* minus (negative)              : N=1 */
+      ARM64CondPL = 5,  /* plus (zero or +ve)            : N=0 */
+
+      ARM64CondVS = 6,  /* overflow                      : V=1 */
+      ARM64CondVC = 7,  /* no overflow                   : V=0 */
+
+      ARM64CondHI = 8,  /* >u   (higher)                 : C=1 && Z=0 */
+      ARM64CondLS = 9,  /* <=u  (lower or same)          : C=0 || Z=1 */
+
+      ARM64CondGE = 10, /* >=s (signed greater or equal) : N=V */
+      ARM64CondLT = 11, /* <s  (signed less than)        : N!=V */
+
+      ARM64CondGT = 12, /* >s  (signed greater)          : Z=0 && N=V */
+      ARM64CondLE = 13, /* <=s (signed less or equal)    : Z=1 || N!=V */
+
+      ARM64CondAL = 14, /* always (unconditional)        : 1 */
+      ARM64CondNV = 15  /* always (unconditional)        : 1 */
+   }
+   ARM64Condcode;
+
+#endif /* ndef __VEX_GUEST_ARM64_DEFS_H */
+
+/*---------------------------------------------------------------*/
+/*--- end                                  guest_arm64_defs.h ---*/
+/*---------------------------------------------------------------*/

commit 191efd2983211fab5eb1ed2791998544b1ae9e0b

arm64: rename guest_SP to guest_XSP so as to avoid a name clash with guest_SP from s390 world.  Also back out the rename of guest_SP to guest_s390_SP that caused s390 build breakage in r2803.
git-svn-id: svn://svn.valgrind.org/vex@2805 8f6e269a-dfd6-0310-a8e1-e2731360e62c

trunk/auxprogs/genoffsets.c
@@ -170,7 +170,7 @@ void foo ( void )
    GENOFFSET(ARM64,arm64,X6);
    GENOFFSET(ARM64,arm64,X7);
    GENOFFSET(ARM64,arm64,X8);
-   GENOFFSET(ARM64,arm64,SP);
+   GENOFFSET(ARM64,arm64,XSP);
    GENOFFSET(ARM64,arm64,PC);
 
    // s390x

trunk/priv/guest_arm64_helpers.c
@@ -1205,13 +1205,13 @@ void LibVEX_GuestARM64_initialise ( /*OUT*/VexGuestARM64State* vex_state )
 Bool guest_arm64_state_requires_precise_mem_exns ( Int minoff, 
                                                    Int maxoff)
 {
-   Int sp_min = offsetof(VexGuestARM64State, guest_SP);
-   Int sp_max = sp_min + 8 - 1;
-   Int pc_min = offsetof(VexGuestARM64State, guest_PC);
-   Int pc_max = pc_min + 8 - 1;
+   Int xsp_min = offsetof(VexGuestARM64State, guest_XSP);
+   Int xsp_max = xsp_min + 8 - 1;
+   Int pc_min  = offsetof(VexGuestARM64State, guest_PC);
+   Int pc_max  = pc_min + 8 - 1;
 
-   if (maxoff < sp_min || minoff > sp_max) {
-      /* no overlap with sp */
+   if (maxoff < xsp_min || minoff > xsp_max) {
+      /* no overlap with xsp */
       if (vex_control.iropt_register_updates == VexRegUpdSpAtMemAccess)
          return False; // We only need to check stack pointer.
    } else {
@@ -1258,7 +1258,7 @@ VexGuestLayout
           .total_sizeB = sizeof(VexGuestARM64State),
 
           /* Describe the stack pointer. */
-          .offset_SP = offsetof(VexGuestARM64State,guest_SP),
+          .offset_SP = offsetof(VexGuestARM64State,guest_XSP),
           .sizeof_SP = 8,
 
           /* Describe the instruction pointer. */

trunk/priv/guest_arm64_toIR.c
@@ -612,7 +612,7 @@ static IRTemp mathREPLICATE ( IRType ty, IRTemp arg, UInt imm )
 #define OFFB_X29      offsetof(VexGuestARM64State,guest_X29)
 #define OFFB_X30      offsetof(VexGuestARM64State,guest_X30)
 
-#define OFFB_SP       offsetof(VexGuestARM64State,guest_SP)
+#define OFFB_XSP      offsetof(VexGuestARM64State,guest_XSP)
 #define OFFB_PC       offsetof(VexGuestARM64State,guest_PC)
 
 #define OFFB_CC_OP    offsetof(VexGuestARM64State,guest_CC_OP)
@@ -715,7 +715,7 @@ static Int offsetIReg64 ( UInt iregNo )
 
 static Int offsetIReg64orSP ( UInt iregNo )
 {
-   return iregNo == 31  ? OFFB_SP  : offsetIReg64(iregNo);
+   return iregNo == 31  ? OFFB_XSP  : offsetIReg64(iregNo);
 }
 
 static const HChar* nameIReg64orZR ( UInt iregNo )

trunk/priv/guest_s390_helpers.c
@@ -154,11 +154,11 @@ LibVEX_GuestS390X_initialise(VexGuestS390XState *state)
 Bool
 guest_s390x_state_requires_precise_mem_exns(Int minoff, Int maxoff)
 {
-   Int lr_min = S390X_GUEST_OFFSET(guest_s390_LR);
+   Int lr_min = S390X_GUEST_OFFSET(guest_LR);
    Int lr_max = lr_min + 8 - 1;
-   Int sp_min = S390X_GUEST_OFFSET(guest_s390_SP);
+   Int sp_min = S390X_GUEST_OFFSET(guest_SP);
    Int sp_max = sp_min + 8 - 1;
-   Int fp_min = S390X_GUEST_OFFSET(guest_s390_FP);
+   Int fp_min = S390X_GUEST_OFFSET(guest_FP);
    Int fp_max = fp_min + 8 - 1;
    Int ia_min = S390X_GUEST_OFFSET(guest_IA);
    Int ia_max = ia_min + 8 - 1;
@@ -203,11 +203,11 @@ VexGuestLayout s390xGuest_layout = {
    .total_sizeB = sizeof(VexGuestS390XState),
 
    /* Describe the stack pointer. */
-   .offset_SP = S390X_GUEST_OFFSET(guest_s390_SP),
+   .offset_SP = S390X_GUEST_OFFSET(guest_SP),
    .sizeof_SP = 8,
 
    /* Describe the frame pointer. */
-   .offset_FP = S390X_GUEST_OFFSET(guest_s390_FP),
+   .offset_FP = S390X_GUEST_OFFSET(guest_FP),
    .sizeof_FP = 8,
 
    /* Describe the instruction pointer. */

trunk/pub/libvex_guest_arm64.h
@@ -76,7 +76,7 @@ typedef
       ULong guest_X28;
       ULong guest_X29;
       ULong guest_X30;     /* link register */
-      ULong guest_SP;
+      ULong guest_XSP;
       ULong guest_PC;
 
       /* 4-word thunk used to calculate N(sign) Z(zero) C(carry,

trunk/pub/libvex_guest_s390x.h
@@ -166,9 +166,9 @@ void LibVEX_GuestS390X_initialise(VexGuestS390XState *);
 /*--- Dedicated registers                                  ---*/
 /*------------------------------------------------------------*/
 
-#define guest_s390_LR guest_r14  /* Link register */
-#define guest_s390_SP guest_r15  /* Stack pointer */
-#define guest_s390_FP guest_r11  /* Frame pointer */
+#define guest_LR guest_r14  /* Link register */
+#define guest_SP guest_r15  /* Stack pointer */
+#define guest_FP guest_r11  /* Frame pointer */
 
 /*---------------------------------------------------------------*/
 /*--- end                                libvex_guest_s390x.h ---*/